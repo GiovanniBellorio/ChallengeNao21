13eeec5400202523c0d05d2cea14688325951d8b
Giovanni Bellorio <gbellorio95@gmail.com>
2020-12-16 15:49:45 +0100

    esempio

---
+++ conta_le_vocali.py
@@ -0,0 +1,30 @@
+# -- doc
+
+'''
+Nome programma: conta_le_vocali.py
+Descrizione   : programma che conta le vocali di una stringa
+Autore        : Giovanni Bellorio
+'''
+
+# -- start
+nome_programma = "conta_le_vocali.py"
+print("Inizio esecuzione", nome_programma)
+
+# -- input
+s = input("Inserisci una stringa: ")
+
+vocali = 0
+
+# -- execute
+i = 0
+while i < len(s):
+    t = s.lower()
+    if t[i] == 'a' or t[i] == 'e' or t[i] == 'i' or t[i] == 'o' or t[i] == 'u':
+        vocali += 1
+    i += 1
+
+# -- output
+print("La stringa inserita contiene:", vocali, "vocali.")
+
+# -- end
+print("Fine esecuzione", nome_programma)
