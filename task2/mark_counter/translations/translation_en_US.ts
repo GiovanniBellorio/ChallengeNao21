39692b27616e9c61f8a7a3c2a9e896d9e95666df
Giovanni Bellorio <gbellorio95@gmail.com>
2021-04-07 15:17:55 +0200

    mark counter

--- task2/mark_counter/translations/translation_en_US.ts
+++ task2/mark_counter/translations/translation_en_US.ts
@@ -1,3 +1,17 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="en_US"/>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/Mark_counter/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Partiamo</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Partiamo</translation>
+        </message>
+    </context>
+</TS>
